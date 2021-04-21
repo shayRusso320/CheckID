eb8a88b813c92d411d64699cbbbb63e7df0195d5
shayRusso320 <66921237+shayRusso320@users.noreply.github.com>
2021-04-21 11:52:13 +0300

    Update main.py
    added a check of each character to determine if they're all digits

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 def check_ID(id_num):
     sum_check = 0
     weight = 1
-    
+
     if len(id_num) != 9:
         print("length is not valid")
-      
-    else
-        for ch in id_num[0:-1]:
-            digit = ord(ch) - ord('0')
-            mult = weight * digit
+        return
 
-            if mult > 9:  # if mult is two digit, then use the sum of the digits instead
-                mult = int(mult / 10) + mult % 10
+    for ch in id_num[0:-1]:
+        digit = ord(ch) - ord('0')
 
-            sum_check += mult
-            weight = 3 - weight  # switching between 1 and 2
+        if digit < 0 or digit > 9:
+            print("character '" + ch + "' is not a number")
+            return
 
-        return ord(id_num[-1]) - ord('0') == 10 - (sum_check % 10)
+        mult = weight * digit
 
+        if mult > 9:  # if mult is two digit, then use the sum of the digits instead
+            mult = int(mult / 10) + mult % 10
 
-print(check_ID("111111111"))
+        sum_check += mult
+        weight = 3 - weight  # switching between 1 and 2
+
+    return ord(id_num[-1]) - ord('0') == 10 - (sum_check % 10)
+
+print(check_ID("325172d987"))
